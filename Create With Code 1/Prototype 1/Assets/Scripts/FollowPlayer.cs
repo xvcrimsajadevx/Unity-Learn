b71fd032881b6bf3bc70f9dc07b27c4ea14fda04
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-01 13:36:41 -0700

    Lesson 1.3 - High Speed Chase (Complete)

---
+++ Create With Code 1/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 offset = new Vector3(0, 7, -9);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Offsets camera behind the player by adding to player's position
+        transform.position = Player.transform.position + offset;
+    }
+}
