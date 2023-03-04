77a0eca612729c27db3884bc7a2f17942961fb6d
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-04 01:20:39 -0700

    Bonus Features 1 - Share Your  Work (Complete*)
    Easy: Obstacle pyramids (Complete)
    Medium: Oncoming vehicles (Complete)
    Hard: Camera switcher (Complete)
    Expert: Local multiplayer

--- Create With Code 1/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Create With Code 1/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,18 +5,33 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     [SerializeField] private GameObject Player;
-    private Vector3 offset = new Vector3(0, 7, -9);
+    private Vector3 offset1 = new Vector3(0, 7, -9);
+    private Vector3 offset2 = new Vector3(0, 5, 0);
+
+    private Vector3 currentOffset;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentOffset = offset1;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+        if (Input.GetButtonDown("SwitchCamera"))
+        {
+            if (currentOffset == offset1)
+            {
+                currentOffset = offset2;
+            }
+            else
+            {
+                currentOffset = offset1;
+            }
+        }
+        
         // Offsets camera behind the player by adding to player's position
-        transform.position = Player.transform.position + offset;
+        transform.position = Player.transform.position + currentOffset;
     }
 }
---
+++ Create With Code 1/Prototype 1/Assets/Scripts/MoveTraffic.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTraffic : MonoBehaviour
+{
+    private float speed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
