206fceeb615d0d2236c32c8f92653612fa81772d
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-04 03:41:37 -0700

    Clean code and variables

--- Create With Code 1/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create With Code 1/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 10f;
+    [SerializeField] private float xRange = 10f;
     public float horizontalInput;
 
     // Start is called before the first frame update
@@ -16,17 +17,20 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Gets player input
         horizontalInput = Input.GetAxis("Horizontal");
 
+        // Moves player
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
-        if (transform.position.x < -10)
+        // Keeps Player inbound
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.y);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.y);
         }
-        else if (transform.position.x > 10)
+        else if (transform.position.x > xRange)
         {
-            transform.position = new Vector3(10, transform.position.y, transform.position.y);
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.y);
         }
     }
 }
