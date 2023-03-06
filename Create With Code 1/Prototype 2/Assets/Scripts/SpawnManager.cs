e128337d96d7dce8d4465396f66afdd90151500d
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-05 23:20:54 -0700

    Spawn animal if S is pressed

--- Create With Code 1/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Create With Code 1/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] animalPrefabs;
+    public int animalIndex;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,9 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+        }
     }
 }
