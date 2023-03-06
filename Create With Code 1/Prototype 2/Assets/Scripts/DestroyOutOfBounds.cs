3ff5bc17597ec4a16a1642a5b1c0feaec1f5e8c9
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-06 05:07:41 -0700

    Trigger a "Game Over" message

--- Create With Code 1/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
+++ Create With Code 1/Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -17,8 +17,14 @@ public class DestroyOutOfBounds : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z > topBound || transform.position.z < lowerBound)
+        if (transform.position.z > topBound)
         {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over!");
+
             Destroy(gameObject);
         }
     }
