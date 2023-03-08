abd545714b72c88518228514e9f68f5fbd051486
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-07 20:35:25 -0700

    Challenge 2 - Play Fetch (Complete)

---
+++ Create With Code 1/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private float spawnTimer;
+    private float spawnDelay = 0.8f;
+
+    public GameObject dogPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spawnTimer += Time.deltaTime;
+
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && spawnTimer >= spawnDelay)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            spawnTimer = 0;
+        }
+    }
+}
