3685d06d61e0d8f95d657ad0a0e57e928fc4a856
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-12 10:28:33 -0600

    Lesson 3.5 - Reset position of background

--- Create With Code 2/Prototype 3/Assets/Scripts/RepeatBackground.cs
+++ Create With Code 2/Prototype 3/Assets/Scripts/RepeatBackground.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class RepeatBackground : MonoBehaviour
 {
+    private Vector3 startPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        startPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.x < startPos.x - 50)
+        {
+            transform.position = startPos;
+        }
     }
 }
