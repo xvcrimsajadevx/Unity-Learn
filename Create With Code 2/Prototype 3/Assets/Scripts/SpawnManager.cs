7a64f888c44080afc2ff67f9a741e730a394ecdb
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-11 22:21:14 -0700

    Lesson 3.1 - Create a spawn manager

---
+++ Create With Code 2/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
