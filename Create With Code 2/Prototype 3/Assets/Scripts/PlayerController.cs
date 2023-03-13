86ad66602e5d350429934546dac2cb70a11d8084
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-12 21:54:24 -0600

    Lesson 3.3 - Keep player from jumping unconscious

--- Create With Code 2/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Create With Code 2/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
 
