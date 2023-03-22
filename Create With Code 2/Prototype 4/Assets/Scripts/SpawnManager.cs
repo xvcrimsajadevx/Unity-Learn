e951fda553a1dbcb776b9300c0a7bc384a9cc937
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-21 19:39:13 -0600

    Lesson 4.2 - Randomly generate spawn position

--- Create With Code 2/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Create With Code 2/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private float spawnRange = 90.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        float spawnRangeX = Random.Range(-spawnRange, spawnRange);
+        float spawnRangeZ = Random.Range(-spawnRange, spawnRange);
+
+        Vector3 spawnPos = new Vector3(spawnRangeX, 0, spawnRangeZ);
+
+        Instantiate(enemyPrefab, spawnPos, enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
