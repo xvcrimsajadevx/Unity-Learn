0163ecb9113dfc7c448bb7e9703607358d1fca5a
LaTora Prince <xvcrimsajadevx@hotmail.com>
2023-03-24 00:35:08 -0600

    4.4 - Increase enemyCount with waves

--- Create With Code 2/Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Create With Code 2/Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,12 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private float spawnRange = 90.0f;
 
     public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(3);
+        SpawnEnemyWave(waveNumber);
     }
 
     // Update is called once per frame
@@ -22,7 +23,9 @@ public class SpawnManager : MonoBehaviour
 
         if (enemyCount == 0 )
         {
-            SpawnEnemyWave(1);
+            waveNumber++;
+
+            SpawnEnemyWave(waveNumber);
         }
     }
 
